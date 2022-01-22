25040903e5ab1522f9c2394a7b519c132832a237
pelumi454 <amoochristianah454@gmail.com>
2022-01-22 13:13:01 -0800

    add custom button to section.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home'
+import Section from './components/Section';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <Home/>
+      {/* <Home /> */}
+      <Section/>
     </div>
   );
 }
