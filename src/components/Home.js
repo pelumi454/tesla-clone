4ac4b1d6b8d5ceab4250dd46130a05696ddfc180
pelumi454 <amoochristianah454@gmail.com>
2022-01-22 13:34:36 -0800

    add react reveal package to section.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import styled from "styled-components"
-import Section from './Section';
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
 
-
-function Home() {
-    return (
+const Home = () => {
+  return (
+    <div>
       <Container>
-        <Section />
-        <Section />
-        <Section />
-        <Section />
+        <Section
+          title="Model S"
+          description="Order Online for   Touchless Delivery"
+          backgroundImg="model.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model Y"
+          description="Order Online for   Touchless Delivery"
+          backgroundImg="model-y.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model X "
+          description="Order Online for   Touchless Delivery"
+          backgroundImg="model-x.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Lowest Cost Solar Panels in America"
+          description="Money-back guarantee"
+          backgroundImg="solar-panel.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn More"
+        />
+        <Section
+          title="Solar for New Roof"
+          description="Solar Roof Costs Less Than a New Roof"
+          backgroundImg="solar-roof.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn more"
+        />
+        <Section
+          title="Accessories"
+          description="Solar Roof Costs Less Than a New Roof"
+          backgroundImg="accessories.jpg"
+          leftBtnText="Shop now"
+        />
       </Container>
-    );
-}
+    </div>
+  );
+};
 
 export default Home;
 
 const Container = styled.div`
-height: 100vh
-`
+  height: 100vh;
+`;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
-
-function Section() {
-    return (
-      <Wrap>
+const Section = ({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) => {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
         <ItemText>
-          <h1>Model S</h1>
-          <p>Order Online for Touchless Delivery</p>
+          <h1>{title}</h1>
+          <p>{description}</p>
         </ItemText>
+      </Fade>
+      <Fade bottom>
         <Buttons>
           <ButtonGroup>
-            <LeftButton>Custom Order</LeftButton>
-            <RightButton>Existing Inventory</RightButton>
+            <LeftButton>{leftBtnText} </LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
           </ButtonGroup>
           <DownArrow src="/images/down-arrow.svg" />
         </Buttons>
-      </Wrap>
-    );
-}
+      </Fade>
+    </Wrap>
+  );
+};
 
 export default Section;
 
@@ -33,15 +43,18 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-//   background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
+
 const ItemText = styled.div`
-  padding: 15vh;
+  padding-top: 15vh;
   text-align: center;
 `;
+
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+
   @media (max-width: 768px) {
     flex-direction: column;
   }
@@ -77,4 +90,3 @@ const DownArrow = styled.img`
 `;
 
 const Buttons = styled.div``;
-
